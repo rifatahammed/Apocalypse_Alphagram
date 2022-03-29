75ca3ed7f2fe8bbc5bb7e8e49de35dd63d57ce99
Rifat Ahammed <reardj007@gmail.com>
2022-03-29 15:14:44 +0600

    Dynamic Star in gun added

--- Source/Shooter_UE4cpp_Udemy/Item.h
+++ Source/Shooter_UE4cpp_Udemy/Item.h
@@ -6,6 +6,17 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+UENUM(BlueprintType)
+enum class EItemRarity : uint8
+{
+	EIR_Damaged UMETA(DisplayName = "Damaged"),
+	EIR_Common UMETA(DisplayName = "Common"),
+	EIR_Uncommon UMETA(DisplayName = "Uncommon"),
+	EIR_Rare UMETA(DisplayName = "Rare"),
+	EIR_Legendary UMETA(DisplayName = "Legendary"),
+
+	EIR_MAX UMETA(DisplayName = "DefaultMAX")
+};
 UCLASS()
 class SHOOTER_UE4CPP_UDEMY_API AItem : public AActor
 {
@@ -37,6 +48,8 @@ protected:
 			UPrimitiveComponent* OtherComp,
 			int32 OtherBodyIndex);
 
+	/** Sets the ActiveStars array of bools based on rarity */
+	void SetActiveStars();
 
 public:	
 	// Called every frame
@@ -66,6 +79,13 @@ private:
 	/** Item count (ammo, etc.) */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	int32 ItemCount;
+
+	/** Item rarity - determines number of stars in Pickup Widget */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	EItemRarity ItemRarity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	TArray<bool> ActiveStars;
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const { return PickupWidget; }
 };
