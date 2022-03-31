b4dcf1f62b317ff83b3a1813b9a1600307ea692a
Rifat Ahammed <reardj007@gmail.com>
2022-03-31 12:36:18 +0600

    Added Curve For Item Z interp

--- Source/Shooter_UE4cpp_Udemy/Item.h
+++ Source/Shooter_UE4cpp_Udemy/Item.h
@@ -106,6 +106,11 @@ private:
 	/** State of the Item */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	EItemState ItemState;
+
+	/** The curve asset to use for the item's Z location when interping */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UCurveFloat* ItemZCurve;
+
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const { return PickupWidget; }
 	FORCEINLINE USphereComponent* GetAreaSphere() const { return AreaSphere; }
