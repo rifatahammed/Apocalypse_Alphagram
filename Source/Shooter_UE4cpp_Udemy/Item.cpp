7abc215ec16c9c2930caa5d85b8c31b5d9dd1ca4
Rifat Ahammed <reardj007@gmail.com>
2022-03-31 17:46:25 +0600

    Inter In X and Y added

--- Source/Shooter_UE4cpp_Udemy/Item.cpp
+++ Source/Shooter_UE4cpp_Udemy/Item.cpp
@@ -17,7 +17,9 @@ AItem::AItem():
 	ZCurveTime(0.7f),
 	ItemInterpStartLocation(FVector(0.f)),
 	CameraTargetLocation(FVector(0.f)),
-	bInterping(false)
+	bInterping(false),
+	ItemInterpX(0.f),
+	ItemInterpY(0.f)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -222,23 +224,23 @@ void AItem::ItemInterp(float DeltaTime)
 		// Scale factor to multiply with CurveValue
 		const float DeltaZ = ItemToCamera.Size();
 
-		//const FVector CurrentLocation{ GetActorLocation() };
-		//// Interpolated X value
-		//const float InterpXValue = FMath::FInterpTo(
-		//	CurrentLocation.X,
-		//	CameraInterpLocation.X,
-		//	DeltaTime,
-		//	30.0f);
-		//// Interpolated Y value
-		//const float InterpYValue = FMath::FInterpTo(
-		//	CurrentLocation.Y,
-		//	CameraInterpLocation.Y,
-		//	DeltaTime,
-		//	30.f);
+		const FVector CurrentLocation{ GetActorLocation() };
+		// Interpolated X value
+		const float InterpXValue = FMath::FInterpTo(
+			CurrentLocation.X,
+			CameraInterpLocation.X,
+			DeltaTime,
+			30.0f);
+		 //Interpolated Y value
+		const float InterpYValue = FMath::FInterpTo(
+			CurrentLocation.Y,
+			CameraInterpLocation.Y,
+			DeltaTime,
+			30.f);
 
 		//// Set X and Y of ItemLocation to Interped values
-		//ItemLocation.X = InterpXValue;
-		//ItemLocation.Y = InterpYValue;
+		ItemLocation.X = InterpXValue;
+		ItemLocation.Y = InterpYValue;
 
 		// Adding curve value to the Z component of the Initial Location (scaled by DeltaZ)
 		ItemLocation.Z += CurveValue * DeltaZ;
