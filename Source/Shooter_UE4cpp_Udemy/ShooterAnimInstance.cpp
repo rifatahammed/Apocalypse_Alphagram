327ebc029ab929ce98ef902f581f8292f18caf63
Rifat Ahammed <reardj007@gmail.com>
2022-04-13 15:46:26 +0600

    Crouch Walking And reload fix

--- Source/Shooter_UE4cpp_Udemy/ShooterAnimInstance.cpp
+++ Source/Shooter_UE4cpp_Udemy/ShooterAnimInstance.cpp
@@ -145,8 +145,23 @@ void UShooterAnimInstance::TurnInPlace()
 		{
 			bTurningInPlace = false;
 		}
+	}
 
-		if (bTurningInPlace)
+	//Set the Recoil Weight
+	if (bTurningInPlace)
+	{
+		if (bReloading)
+		{
+			RecoilWeight = 1.f;
+		}
+		else
+		{
+			RecoilWeight = 0.f;
+		}
+	}
+	else // not turning in place
+	{
+		if (bCrouching)
 		{
 			if (bReloading)
 			{
@@ -154,34 +169,20 @@ void UShooterAnimInstance::TurnInPlace()
 			}
 			else
 			{
-				RecoilWeight = 0.f;
+				RecoilWeight = 0.1f;
 			}
 		}
-		else // not turning in place
+		else
 		{
-			if (bCrouching)
+			if (bAiming || bReloading)
 			{
-				if (bReloading)
-				{
-					RecoilWeight = 1.f;
-				}
-				else
-				{
-					RecoilWeight = 0.1f;
-				}
+				RecoilWeight = 1.f;
 			}
 			else
 			{
-				if (bAiming || bReloading)
-				{
-					RecoilWeight = 1.f;
-				}
-				else
-				{
-					RecoilWeight = 0.5f;
-				}
-
+				RecoilWeight = 0.5f;
 			}
+
 		}
 	}
 }
