dde2449bb7f427b756556730e80809457b7804e8
Rifat Ahammed <reardj007@gmail.com>
2022-05-17 15:16:40 +0600

    Dont re Attack after Character is dead

--- Source/Shooter_UE4cpp_Udemy/ShooterCharacter.cpp
+++ Source/Shooter_UE4cpp_Udemy/ShooterCharacter.cpp
@@ -21,6 +21,8 @@
 #include "Shooter_UE4cpp_Udemy.h"
 #include "BulletHitInterface.h"
 #include "Enemy.h"
+#include "EnemyController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter():
@@ -146,6 +148,15 @@ AShooterCharacter::AShooterCharacter():
 		{
 			Health = 0.f;
 			Die();
+
+			auto EnemyController = Cast<AEnemyController>(EventInstigator);
+			if (EnemyController)
+			{
+				EnemyController->GetBlackboardComponent()->SetValueAsBool(
+					FName(TEXT("CharacterDead")),
+					true
+				);
+			}
 		}
 		else
 		{
